70f7dad8da5262b6dce6e582072c2f6d415d88eb
aundra miller and jared beckler <miller.aundra@gmail.com>
2016-02-10 16:13:44 -0800

    create views files and initial tamagotchi functionality

--- app/app.php
+++ app/app.php
@@ -1,16 +1,70 @@
 <?php
 
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Job.php";
+    require_once __DIR__."/../src/Tamagotchi.php";
+
+    session_start();
+    if (empty($_SESSION['living_tamagotchi'])) {
+        $_SESSION['living_tamagotchi'] = array();
+    }
 
     $app = new Silex\Application();
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('new_tamagotchi.html.twig');
+    });
+
+    $app->post("/tamagotchi", function() use ($app) {
+        $new_tamagotchi = new Tamagotchi($_POST['name'], 6, 21, 15);
+        $new_tamagotchi->save();
+        return $app['twig']->render('tamagotchi_status.html.twig', array('new_tamagotchi' => $new_tamagotchi));
+    });
+
+    $app->post("/feed", function() use ($app) {
+        $tamagotchi = Tamagotchi::getAll();
+        $my_food = $tamagotchi[0]->getFood();
+        $tamagotchi[0]->setFood($my_food + 6);
+        return $app['twig']->render('fed.html.twig');
+    });
+
+
+
+
+
+
+
+
+    // $app->post("/care", function() use ($app) {
+    //     if($_POST['action'] == 'feed') {
+    //         $my_food = $new_tamagotchi->getFood();
+    //         $new_tamagotchi->setFood($my_food + 6);
+    //
+    //     } elseif ($_POST['action'] == 'sleep') {
+    //         $my_food = $new_tamagotchi->getFood();
+    //         $new_tamagotchi->setFood($my_food - 1);
+    //
+    //     } elseif ($_POST['action'] == 'attention') {
+    //         $my_food = $new_tamagotchi->getFood();
+    //         $new_tamagotchi->setFood($my_food - 1);
+    //
+    //     } else {
+    //
+    //     }
+    //
+    // });
+
+    // $app->post("/tamagotchi", function() use ($app) {
+    //     if($_POST['action'] == 'feed') {
+    //         ldksjflkdsjl
+    //     } elseif ($_POST['action'] == 'sleep'){
+    //         dfsdfaf
+    //     }
+    // })
 
 
     return $app;
-
-
-
-
-
  ?>
---
+++ src/Tamagotchi.php
@@ -0,0 +1,69 @@
+<?php
+
+class Tamagotchi {
+    private $name;
+    private $food;
+    private $sleep;
+    private $attention;
+
+    function __construct($set_name, $set_food, $set_sleep, $set_attention)
+    {
+        $this->name = $set_name;
+        $this->food = $set_food;
+        $this->sleep = $set_sleep;
+        $this->attention = $set_attention;
+    }
+
+    function setName($set_name)
+    {
+        $this->name = $set_name;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setFood($set_food)
+    {
+        $this->food = $set_food;
+    }
+
+    function getFood()
+    {
+        return $this->food;
+    }
+
+    function setSleep($set_sleep)
+    {
+        $this->sleep = $set_sleep;
+    }
+
+    function getSleep()
+    {
+        return $this->sleep;
+    }
+
+    function setAttention($set_attention)
+    {
+        $this->attention = $set_attention;
+    }
+
+    function getAttention()
+    {
+        return $this->attention;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['living_tamagotchi'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['living_tamagotchi'];
+    }
+
+}
+
+?>
